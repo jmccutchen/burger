0f5ff722b24aa6b6ae2017fc89820a1d597f2fbd
jmccutchen <purplegrits@yahoo.com>
2020-01-05 07:41:20 -0500

    added Heroku link

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PW,
-  database: "burger_db"
-});
+const connection;
 
-connection.connect(function(err) {
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: "burger_db"
+  });
+};
+
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
