8fe8e351d1211a4217a6427e0d68ab43624ab148
jmccutchen <purplegrits@yahoo.com>
2020-01-05 07:47:30 -0500

    fixed connection file

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,13 @@
 const mysql = require("mysql");
 
-const connection;
+const connection = mysql.createConnection({
+  host: "mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "hk819zn2xp9inhii",
+  password: "ok0yg97lbe9jhdby",
+  database: "pz1ald4bxxsulh39"
+});
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PW,
-    database: "burger_db"
-  });
-};
 
 connection.connect(function (err) {
   if (err) {
